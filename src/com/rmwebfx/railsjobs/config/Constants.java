5cbf958ecd396b3e7f4e7aed89be3eda22e309be
Mike Chandler <mike@rmwebfx.com>
2012-01-08 14:41:13 -0800

    Set the stage to persist Location for job searches.

---
+++ src/com/rmwebfx/railsjobs/config/Constants.java
@@ -0,0 +1,6 @@
+package com.rmwebfx.railsjobs.config;
+
+public class Constants {
+	public static String indeedURL = "http://api.indeed.com/ads/apisearch";
+	public static String publisherID = "7408574424678082";
+}
