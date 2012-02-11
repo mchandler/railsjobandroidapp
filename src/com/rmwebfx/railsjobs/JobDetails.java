d2a45ebe36027dc09760adb2feacc63eab3e8f32
Mike Chandler <mike@rmwebfx.com>
2012-02-11 08:30:06 -0800

    Setting the stage to use a web view for job details.

---
+++ src/com/rmwebfx/railsjobs/JobDetails.java
@@ -0,0 +1,14 @@
+package com.rmwebfx.railsjobs;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class JobDetails extends Activity {
+    
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.jobdetails);
+        
+    }    
+}
